622af6bc291a391b51272a27327891eb86ecf4ec
Chua Jie Sheng <chuajiesheng@gmail.com>
2013-06-24 11:52:34 -0400

    added done and delete function

---
+++ todo.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python -tt
+# todo.py
+
+# vim: smartindent expandtab tabstop=4 shiftwidth=4 softtabstop=4
+import sys
+
+# flask import
+import flask
+from flask.ext.sqlalchemy import SQLAlchemy
+app = flask.Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+db = SQLAlchemy(app)
+# end flask import
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String)
+    done = db.Column(db.Boolean)
+
+def main():
+    cmd = sys.argv[1]
+    if cmd == 'add':
+        if len(sys.argv) > 2:
+            t = sys.argv[2]
+            db.session.add(Item(text=t))
+            db.session.commit()
+            print 'adding', t
+    elif cmd == 'delete':
+        id = int(sys.argv[2])
+        item = Item.query.get(id)
+        db.session.delete(item)
+        db.session.commit()
+    elif cmd == 'done':
+        id = int(sys.argv[2])
+        item = Item.query.get(id)
+        item.done = True
+        db.session.commit()
+    elif cmd == 'list':
+        for item in Item.query.all():
+            status = '[x]' if item.done else '[ ]'
+            print item.id, status, item.text
+    else:
+        print 'unknown command'
+
+if __name__ == '__main__':
+    app.app_context().push()
+    db.create_all()
+    main()
+
